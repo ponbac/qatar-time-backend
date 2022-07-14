6af0d988b450cf240b1b1aa44b6a8112931f29ef
ponbac <root@ponbac.xyz>
2022-07-14 10:35:12 +0200

    oopsie

--- realtime_server.py
+++ realtime_server.py
@@ -28,20 +28,16 @@ def groups_callback(payload):
 
 
 def main():
-    URL = f"wss://{conf.SUPABASE_ID}.supabase.co/realtime/v1/websocket?apikey={conf.SUPABASE_KEY}&vsn=1.0.0"
-    s = Socket(URL)
-    s.connect()
-
-    games_channel = s.set_channel("realtime:public:games")
-    games_channel.join().on("UPDATE", game_callback)
-    # groups_channel = s.set_channel("realtime:public:groups")
-    # groups_channel.join().on("UPDATE", groups_callback)
-    s.listen()
-
-
-if __name__ == "__main__":
     try:
-        main()
+        URL = f"wss://{conf.SUPABASE_ID}.supabase.co/realtime/v1/websocket?apikey={conf.SUPABASE_KEY}&vsn=1.0.0"
+        s = Socket(URL)
+        s.connect()
+
+        games_channel = s.set_channel("realtime:public:games")
+        games_channel.join().on("UPDATE", game_callback)
+        # groups_channel = s.set_channel("realtime:public:groups")
+        # groups_channel.join().on("UPDATE", groups_callback)
+        s.listen()
     except Exception as e:
         print(e)
         if e == KeyboardInterrupt:
@@ -50,3 +46,7 @@ if __name__ == "__main__":
         else:
             print("Something went wrong, restarting...")
             main()
+
+
+if __name__ == "__main__":
+    main()
