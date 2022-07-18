3a436ab33f1efd4e356bc68224984d823c9ce3bb
ponbac <root@ponbac.xyz>
2022-07-18 17:36:22 +0200

    added back restart func

--- realtime_server.py
+++ realtime_server.py
@@ -29,24 +29,24 @@ def groups_callback(payload):
 
 
 def main():
-    # try:
-    URL = f"wss://{conf.SUPABASE_ID}.supabase.co/realtime/v1/websocket?apikey={conf.SUPABASE_KEY}&vsn=1.0.0"
-    s = Socket(URL)
-    s.connect()
+    try:
+        URL = f"wss://{conf.SUPABASE_ID}.supabase.co/realtime/v1/websocket?apikey={conf.SUPABASE_KEY}&vsn=1.0.0"
+        s = Socket(URL)
+        s.connect()
 
-    games_channel = s.set_channel("realtime:public:games")
-    games_channel.join().on("UPDATE", game_callback)
-    # groups_channel = s.set_channel("realtime:public:groups")
-    # groups_channel.join().on("UPDATE", groups_callback)
-    s.listen()
-    # except Exception as e:
-    #     print(e)
-    #     if e == KeyboardInterrupt:
-    #         print("Exiting...")
-    #         os._exit(0)
-    #     else:
-    #         print("Something went wrong, restarting...")
-    #         main()
+        games_channel = s.set_channel("realtime:public:games")
+        games_channel.join().on("UPDATE", game_callback)
+        # groups_channel = s.set_channel("realtime:public:groups")
+        # groups_channel.join().on("UPDATE", groups_callback)
+        s.listen()
+    except Exception as e:
+        print(e)
+        if e == KeyboardInterrupt:
+            print("Exiting...")
+            os._exit(0)
+        else:
+            print("Something went wrong, restarting...")
+            main()
 
 
 if __name__ == "__main__":
